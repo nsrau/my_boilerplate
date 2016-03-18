f10e59992dc6ae5b8f930cca6e26db11ba40ceda
Newton Urbanetz <new.urbanetz@gmail.com>
2016-03-18 13:17:49 +0100

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    clean: {
+      js: ["js/app.min.js"],
+      css: ["css/style.min.css"]
+    },
+    concat: {
+      js: {
+        "src": "js/build/**/*.js",
+        "dest": "js/app.js"
+      },
+      css: {
+        "src": 'css/**/*.css',
+        "dest": 'css/style.css'
+      }
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions']
+      },
+      your_target: {
+        "src": 'css/style.css'
+      }
+    },
+    uglify: {
+      js: {
+        "src": "js/app.js",
+        "dest": "js/app.min.js"
+      }
+    },
+    cssmin: {
+      css: {
+        src: 'css/style.css',
+        dest: 'css/style.min.css'
+      }
+    },
+    clean: {
+      js: ["js/app.js"],
+      css: ["css/style.css"]
+    }
+
+  });
+
+  // Load required modules
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Task definitions
+  grunt.registerTask('default', ['clean:js', 'clean:css', 'concat', 'autoprefixer', 'uglify:js', 'cssmin', 'clean:js', 'clean:css']);
+};
---
+++ js/build/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  console.log('ready')
+});
+
+$(window).load(function() {
+  console.log('load')
+});
